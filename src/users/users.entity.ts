a606a6a651d8e764b7a96272214ec037ab2c0832
sig-code <m.takku29@gmail.com>
2022-06-14 20:09:34 +0900

    UserEntityを定義

---
+++ src/users/users.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 32, nullable: true })
+  name: string;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
