30d27e0045bccdc6bb0e83c7f8ba46a1b37c15db
sig-code <m.takku29@gmail.com>
2022-06-14 19:58:43 +0900

    users関連のファイルを整理

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 import * as ormConfig from '../ormconfig';
 
 @Module({
